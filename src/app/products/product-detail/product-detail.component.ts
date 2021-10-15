4f2d879646413d71ae16a7c556f49707e6f6d1db
ldex <laurent.duveau+git@gmail.com>
2021-10-15 11:40:07 -0400

    Fin du workshop RxJS

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { filter, map, startWith, switchMap } from 'rxjs/operators';
 
 import { Product } from '../product.interface';
 import { FavouriteService } from '../favourite.service';
 import { ProductService } from '../product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -26,17 +27,36 @@ export class ProductDetailComponent implements OnInit {
     this.router.navigateByUrl('/products');
   }
 
+  private idParamFromRoute$: Observable<number> = this.activatedRoute.params.pipe(
+    filter(routeParams => routeParams.hasOwnProperty("id")), // Is there an id parameter ?
+    map(routeParams => routeParams["id"]), // get the id parameter
+    filter(id => !isNaN(id)), // filter numeric values ("9000" or 9000)
+    map(id => Number(id)) // convert to number ("9000" -> 9000)
+  ); // This will only emit if a valid id parameter is found!
+
+  public product$: Observable<Product> = this.idParamFromRoute$.pipe(
+    startWith(0), // No id parameter found or id param is not a valid number => default id to 0
+    switchMap(
+      id => this
+              .productService
+              .products$
+              .pipe(
+                map(products => products.find(p => p.id === id) ?? {} as Product), // no product found (or id == 0) returns an empty object
+              )
+    )
+  );
+
   ngOnInit(): void {
-    const id = + this.activatedRoute.snapshot.params['id'];
-    this
-      .productService
-      .products$
-      .pipe(
-        map(products => products.find(p => p.id === id))
-      )
-      .subscribe(
-        product => this.product = product
-      )
+    // const id = + this.activatedRoute.snapshot.params['id'];
+    // this
+    //   .productService
+    //   .products$
+    //   .pipe(
+    //     map(products => products.find(p => p.id === id))
+    //   )
+    //   .subscribe(
+    //     product => this.product = product
+    //   )
   }
 
 }
